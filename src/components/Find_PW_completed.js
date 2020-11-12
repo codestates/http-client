44bd0445ef1795d66e521d65ebca8521b0fd2cc8
Jaehoon Lee <john@Jaehoonui-MacBookPro.local>
2020-11-12 22:27:36 +0900

    [client] add Find_PW_completed.js

---
+++ src/components/Find_PW_completed.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+class CompletedFindPw extends React.Component {
+
+  render() {
+    return (
+      <div className='modal_signUp'>
+
+        <div className='modal_overlay'></div>
+        <div className='modal_content'>
+          <h2>e-mail</h2>
+
+          <div className='container'>
+
+            <h2>PW</h2>
+            <div className='find_pw_box'>
+              <div className='information'>
+                {`귀하의 PW은 ${1234}입니다.`}  {/* 재성님께서 멘트 통째로 보내주기로 함. */}
+              </div>
+            </div>
+
+
+
+
+            <NavLink to='' className='signUp_link'>
+              <button className='signUp_btn'>로그인 페이지로 돌아가기</button>
+            </NavLink>
+
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default CompletedFindPw;
