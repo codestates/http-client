b1792c1e9a81c08fa242d633d1c90adff88eadc8
Siwoo Baek <whitelatte46@gmail.com>
2020-11-13 00:34:42 +0900

    [client] fix routes/* to make routes of each component

--- src/routes/ToDo.js
+++ src/routes/ToDo.js
@@ -1,116 +1,134 @@
 import React from "react";
 import axios from "axios";
+
+// components
 import List from "../components/List";
-import "./ToDo.css"
+import SignInModal from "../components/SignIn";
+import SignUpModal from "../components/SignUp";
 
-import SignInModal from "../components/SignIn"
-import SignUpModal from "../components/SignUp"
+import "./ToDo.css";
 
-
-// fake data... 서버 API 연동 완료 시 삭제해야 함
-let list = [
-    { id: 0, contents: "쇼미더머니9 보기", startDate: "2020-11-02", complete: true, important: false },
-    { id: 1, contents: "그것이알고싶다 보기", startDate: "2020-11-02", complete: false, important: false },
-    { id: 2, contents: "한강 산책하기", startDate: "2020-11-04", complete: true, important: true },
-    { id: 3, contents: "합정 북카페 가기", startDate: "2020-11-04", complete: true, important: true },
-    { id: 4, contents: "오목집 족발 먹기", startDate: "2020-11-07", complete: false, important: false },
-    { id: 5, contents: "베스킨라빈스 먹기", startDate: "2020-11-07", complete: true, important: false },
-    { id: 6, contents: "라떼 간식 사기", startDate: "2020-11-08", complete: false, important: true },
-];
-
-
-
+// test data
+import todoList from "../test_data_list.json";
 
 class ToDo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            doWhat: "",
-            doWhen: "",
-            err: "",
-            important: false,
-            list: []
-        }
-    }
-
-    // 1. 컴포넌트 진입하자마자 전체 목록 가져오기
-    getList = async () => {
-        // const list = await axiox.get("http://localhost:8000/main"); // 노드서버 구축완료시 활성화 & 테스트
-        this.setState({ list: list });
-    }
-
-    // 2. Life cycle에 반영
-    componentDidMount() {
-        this.getList()
-    }
-
-    // 3. 일정입력
-    handleInputValue = (key) => (e) => {
-        this.setState({ [key]: e.target.value });
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: null,
+      startDate: null,
+      important: false,
+      complete: false,
+      err: "",
+      todoList: [],
     };
-    inputToDo = async () => {
-        const isImportant = (e) => {
-            this.setState({ important: e.target.important })
-        }
+  }
 
-        const { doWhat, doWhen, err, important, list } = this.state;
-        if (!this.state.doWhat || !this.state.doWhen) {
-            this.setState({ err: "모든 항목은 필수입니다" })
-        }
-        // const input = await axios.post("http://localhost:8000/todo", {
-        //     contents: doWhat,
-        //     startDate: doWhen,
-        // })  // 노드서버 구축완료시 활성화 & 테스트
+  // 1. 컴포넌트 진입하자마자 전체 목록 가져오기
+  getList = async () => {
+    // const list = await axios.get("http://localhost:8000/main"); // 노드서버 구축완료시 활성화 & 테스트
+    await this.setState({ todoList: todoList });
+  };
 
-        const result = await list.push({ contents: this.state.doWhat, startDate: this.state.doWhen, important: this.state.important })
-        this.props.history.push("/todo");
+  // 2. Life cycle에 반영
+  componentDidMount() {
+    this.getList();
+  }
+
+  // 3. 일정입력
+  handleInputValue = (key) => (e) => {
+    this.setState({ [key]: e.target.value });
+  };
+  makeItImportant = () => {
+    this.setState({ important: true });
+  };
+  inputToDo = async () => {
+    const { content, startDate, err, important, todoList } = this.state;
+    if (!this.state.content || !this.state.startDate) {
+      this.setState({ err: "모든 항목은 필수입니다" });
+    } else {
+      // const todoList = await axios.post("http://localhost:8000/todo", {
+      //     content: content,
+      //     startDate: startDate,
+      // })  // 노드서버 구축완료시 활성화 & 테스트
+      todoList.push({
+        content: this.state.content,
+        startDate: this.state.startDate,
+        important: this.state.important,
+        complete: this.state.complete,
+      });
     }
+    this.props.history.push("/");
+  };
 
-    // 4. 컴포넌트 최종내용 렌더링
-    render() {
-        const { doWhat, doWhen, err, important, list } = this.state;
-        return (
-            <>
+  // 4. 컴포넌트 최종내용 렌더링
+  render() {
+    const {
+      content,
+      startDate,
+      err,
+      important,
+      complete,
+      todoList,
+    } = this.state;
+    return (
+      <>
+        <SignInModal />
 
-
-                <SignInModal />
-
-
-
-                <form className="container-input" onSubmit={(e) => e.preventDefault()}>
-                    <div className="checkbox">
-                        <input type="checkbox" important={important} onChange={this.isImportant} alt="중요해..?" />
-                    </div>
-                    <div className="inputbox">
-                        <div className="input-when">
-                            <span className="input-what-description">언제할까?</span>
-                            <input className="input-what-field" type='date' onChange={this.handleInputValue("doWhen")}></input>
-                        </div>
-                        <div className="input-what">
-                            <span className="input-when-description">뭘할까?</span>
-                            <input className="input-when-field" type="text" onChange={this.handleInputValue("doWhat")}></input>
-                        </div>
-                    </div>
-                    <button className="input-button" type='submit' onClick={this.inputToDo}>입력!</button>
-                    <div className="alert-box">{err !== "" ? err : null}</div>
-                </form>
-                <section className="container-list">
-                    <div className="todo">
-                        {list.map(onedayTodo => (
-                            <List
-                                key={onedayTodo.id}
-                                id={onedayTodo.id}
-                                contents={onedayTodo.contents}
-                                startDate={onedayTodo.startDate}
-                                complete={onedayTodo.complete}
-                                important={onedayTodo.important}
-                            />
-                        ))}
-                    </div>
-                </section>
-            </>
-        )
-    }
+        <form className="container-input" onSubmit={(e) => e.preventDefault()}>
+          <div className="chkbox">
+            <button
+              className="check-important"
+              important={{ important: true }}
+              onClick={this.makeItImportant}
+            >
+              &#11088;
+            </button>
+          </div>
+          <div className="inputbox">
+            <div className="input-when">
+              <span className="input-what-description">언제할까?</span>
+              <input
+                className="input-what-field"
+                type="date"
+                onChange={this.handleInputValue("startDate")}
+              ></input>
+            </div>
+            <div className="input-what">
+              <span className="input-when-description">뭘할까?</span>
+              <input
+                className="input-when-field"
+                type="text"
+                onChange={this.handleInputValue("content")}
+              ></input>
+            </div>
+          </div>
+          <button
+            className="input-button"
+            type="submit"
+            onClick={this.inputToDo}
+          >
+            입력!
+          </button>
+          <div className="alert-box">{err !== "" ? err : null}</div>
+        </form>
+        <section className="container-list">
+          <div className="todo">
+            {todoList.map((onedayTodo, i) => (
+              <List
+                key={i}
+                id={onedayTodo.id}
+                content={onedayTodo.content}
+                startDate={onedayTodo.startDate}
+                complete={onedayTodo.complete}
+                important={onedayTodo.important}
+              />
+            ))}
+          </div>
+        </section>
+      </>
+    );
+  }
 }
 
 export default ToDo;
