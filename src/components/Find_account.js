ea93d9bdcd68f02d12a4fb98a5b60a75d02c7bf9
Jaehoon Lee <john@Jaehoonui-MacBookPro.local>
2020-11-12 15:58:07 +0900

    [Client] add Find_account.js

---
+++ src/components/Find_account.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+class FindAccount extends React.Component {
+
+  render() {
+    return (
+      <div className='modal_signUp'>
+
+        <div className='modal_overlay'></div>
+        <div className='modal_content'>
+          <h2>e-mail 찾기</h2>
+
+          {/* <img src='https://gdimg.gmarket.co.kr/1496139073/still/600?ver=1537817021'></img> */}
+
+
+          <div className='container'>
+
+
+
+            <div className='find_e-mail_box'>
+              <div>
+                <span>고객명</span>
+                <input type='text'></input>
+              </div>
+
+              <div>
+                <span>연락처</span>
+                <input type='text'></input>
+              </div>
+
+            </div>
+            <NavLink to='/userinformaion'>
+              <button className='findBtn'>e-mail 찾기</button>
+            </NavLink>
+            <div className='line'> </div>
+
+            <h2>PW 찾기</h2>
+            <div className='find_pw_box'>
+
+              <div className='inputInfo'>
+                <span className='email_span'>e-mail</span>
+                <input type='email'></input>
+
+              </div>
+              <div>
+                <span>고객명</span>
+                <input type='text'></input>
+              </div>
+
+              <div>
+                <span>연락처</span>
+                <input type='text'></input>
+
+              </div>
+
+            </div>
+
+            <NavLink to='/userinformaion'>
+              <button className='findBtn'>PW 찾기</button>
+            </NavLink>
+
+
+            <NavLink to='' className='signUp_link'>
+              <button className='signUp_btn'>회원 가입</button>
+            </NavLink>
+
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default FindAccount;
