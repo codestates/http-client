14923b20732a8535fe18ab9302e32f9472ac88ee
Jaehoon Lee <john@Jaehoonui-MacBookPro.local>
2020-11-13 20:18:25 +0900

    strange

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,7 +2,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import user from '../test_data_user.json'
-import axios from "axios";
+import axios from 'axios'
+
+
 
 class SignInModal extends React.Component {
 
@@ -67,81 +69,55 @@ class SignInModal extends React.Component {
         console.log(user)
 
     }
-};
-
-// render() {
-//     return (
-//         <div className="modal hidden">
-//             <div className="modal_overlay"></div>
-//             <div className="modal_content">
-//                 <h1>너의 시간을 겟~⭐️</h1>
-
-//                 <div className="container">
-//                     <div className="signUp_div">
-//                         <NavLink to="/signup" className="signUp_link">
-//                             아직 회원이 아니신가요?
-//               </NavLink>
-//                         {/* <div onClick={this.handleClick} onChange={this.moveSignUp} id="to_signUp">아직 회원이 아니신가요?</div> */}
-//                     </div>
 
 
-render() {
+    render() {
+        return (
+            <div className='modal hidden'>
+                <div className='modal_overlay'></div>
+                <div className='modal_content'>
 
-    return (
-
-        <div className='modal hidden'>
-
-
-            <div className='modal_overlay'></div>
-            <div className='modal_content'>
-                <h1>너의 시간을 겟~⭐️</h1>
-
-                <div className='container'>
-
-                    <div className='signUp_div'>
-                        <NavLink to='/signup' className='signUp_link'>아직 회원이 아니신가요?</NavLink>
-                        {/* <div onClick={this.handleClick} onChange={this.moveSignUp} id="to_signUp">아직 회원이 아니신가요?</div> */}
-                    </div>
-
-                    {/* <img src='https://gdimg.gmarket.co.kr/1496139073/still/600?ver=1537817021'></img> */}
-
-                    <img id='sign_in_img' src='https://t1.daumcdn.net/cfile/tistory/992C413B5D2ACF7C1D'></img>
-
-
-                    <div className='container1'>
-
-                        <div className='email_div'>
-                            <span className='email_span'>e-mail</span>
-                            <input type='email' onChange={this.hadleInputValue("email")}></input>
+                    <h1>너의 시간을 겟~⭐️</h1>
+                    <div className='container'>
+                        <div className='signUp_div'>
+                            <NavLink to='/signup' className='signUp_link'>아직 회원이 아니신가요?</NavLink>
+                            {/* <div onClick={this.handleClick} onChange={this.moveSignUp} id="to_signUp">아직 회원이 아니신가요?</div> */}
                         </div>
 
-                        <div className='PW_div'>
-                            <span>PW</span>
-                            <input type='password' onChange={this.hadleInputValue("password")}></input>
+                        {/* <img src='https://gdimg.gmarket.co.kr/1496139073/still/600?ver=1537817021'></img> */}
+                        <img id='sign_in_img' src='https://t1.daumcdn.net/cfile/tistory/992C413B5D2ACF7C1D'></img>
+
+                        <div className='container1'>
+                            <div className='email_div'>
+                                <span className='email_span'>e-mail</span>
+                                <input type='email' onChange={this.hadleInputValue("email")}></input>
+                            </div>
+                            <div className='PW_div'>
+                                <span>PW</span>
+                                <input type='password' onChange={this.hadleInputValue("password")}></input>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='findAccount_span'>
-                        <span>
-                            <NavLink to='/findaccount' className='findAccount_link'>e-mail | PW 찾기</NavLink>
-                        </span>
-                    </div>
+                        <div className='findAccount_span'>
+                            <span>
+                                <NavLink to='/findaccount' className='findAccount_link'>e-mail | PW 찾기</NavLink>
+                            </span>
+                        </div>
 
-                    <div>
-                        {/* <NavLink to="/todo"> */}
-                        <button className='loginButton' type='submit' onClick={this.handleSignIn}>로그인</button>
-                        {/* </NavLink> */}
                         <div>
-                            <button className='loginButton'>Github 로그인</button>
+                            {/* <NavLink to="/todo"> */}
+                            <button className='loginButton' type='submit' onClick={this.handleSignIn}>로그인</button>
+                            {/* </NavLink> */}
+                            <div>
+                                <button className='loginButton'>Github 로그인</button>
+                            </div>
+                            <div className='alert-box'>{this.state.errorMessage}</div>
                         </div>
-                        <div className='alert-box'>{this.state.errorMessage}</div>
                     </div>
-
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
 
 export default SignInModal;
