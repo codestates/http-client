a5085e0fb25189b68d074c4de8c696ad732f0d8a
Jaehoon Lee <john@Jaehoonui-MacBookPro.local>
2020-11-14 15:29:03 +0900

    [clietn] fix Find_Email_completed.js

--- src/components/Find_Email_completed.js
+++ src/components/Find_Email_completed.js
@@ -5,15 +5,25 @@ import { NavLink } from 'react-router-dom'
 import user from '../test_data_user.json';
 
 class CompletedFindEmail extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: ''
-    }
-    console.log(this.props.history.location)
-  }
+  // constructor(props
+  // super(props)
+  // this.state = {
+  //   userName: '',
+  //   email: ''
+  // }
+
+  // }
+
+  // handleInputEmailInformation = () => {
+
+  //   this.setState ({
+  //     userName: this.props.history.location.state.userName,
+  //     email: this.props.history.location.state.email
+  //   })
+  // }
 
   render() {
+    console.log(this.props.history.location)
     return (
       <div className='modal'>
 
@@ -25,7 +35,7 @@ class CompletedFindEmail extends React.Component {
 
             <div className='find_e-mail_box'>
               <div className='information'>
-                {`귀하의 e-mail은 성유리gmail.com입니다.`}  {/* 재성님께서 멘트 통째로 보내주기로 함. */}
+                {`${this.props.history.location.state.userName}님의 e-mail은 ${this.props.history.location.state.email}입니다.`}  {/* 재성님께서 멘트 통째로 보내주기로 함. */}
               </div>
             </div>
 
