e40675158e230ad536a0dc8c6fa14179582f776c
Siwoo Baek <whitelatte46@gmail.com>
2020-11-18 04:09:03 +0900

    [client] fix EditTodo component

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,32 +1,43 @@
-import React, { useState, useCallback } from "react";
+import React, { useCallback } from "react";
 import { MdModeEdit } from "react-icons/md";
-import "./EditTodo.scss";
 
 const EditTodo = ({ editContent }) => {
-  const [editedOne, setEditedOne] = useState("");
+  const [editedOne, setEditedOne] = useState({ content: "", err: "" });
+
   const onChange = (e) => {
     setEditedOne({ [e.target.key]: e.target.value });
   };
-  const onClick = useCallback(() => {
-    editContent(editedOne); // ToDo 컴포넌트에 수정내용 반영
-    setEditedOne(""); // 입력폼 초기화
-  }, [editContent, editedOne]);
+
+  const onSubmit = useCallback(
+    (e) => {
+      if (editedOne.content === "") {
+        setEditedOne({ err: "고칠게 없는가..." });
+      } else {
+        editContent(editedOne); // ToDo 컴포넌트에 수정내용 반영
+        setEditedOne({ content: "", err: "" }); // 입력폼 초기화
+        e.preventDefault(); // submit 이벤트로 인한 새로고침 방지
+      }
+    },
+    [editContent, editedOne]
+  );
+
   return (
     <>
-      <form className="edit-todo">
+      <form className="edit-todo" onSubmit={onSubmit}>
         <input
           className="editedOne"
-          placeholder="수정할 글을 써주세요"
+          placeholder={todos.content}
           name="content"
-          value={editedOne}
+          value={editedOne.content}
           onChange={onChange}
         />
       </form>
-      <button onClick={onClick}>
+      <button type="submit">
         <MdModeEdit />
       </button>
       <div>{editedOne.err !== "" ? editedOne.err : null}</div>
     </>
   );
 };
+
 export default EditTodo;
